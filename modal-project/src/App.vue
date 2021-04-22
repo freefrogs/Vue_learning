2b9ca418923ac6c44484bb839a14fec60b233ba0
Justyna Biernacka <justyna.biernacka.1986@gmail.com>
2021-04-22 13:07:44 +0200

    Add Modal component

--- modal-project/src/App.vue
+++ modal-project/src/App.vue
@@ -1,17 +1,45 @@
 <template>
-  <h1>Hello world</h1>
+  <h1>{{ title }}</h1>
+  <input type="text" ref="name">
+  <button @click="handleClick">click me</button>
+  <div v-if="showModal">
+    <Modal theme="sale" @close="toggleModal">
+      <template v-slot:links>
+        <a href="#">watch intro</a>
+        <a href="#">read more</a>
+      </template>
+      <h2>{{ header }}</h2>
+      <p>{{ content }}</p>
+    </Modal>
+  </div>
+  <br>
+  <br>
+  <button @click="toggleModal">Open modal</button>
 </template>
 
 <script>
+import Modal from './components/Modal'
+
 export default {
   name: 'App',
+  components: {
+    Modal,
+  },
+  data() {
+    return {
+      title: 'Hello world',
+      header: 'Sign up!',
+      content: 'bla, bla, bla',
+      showModal: false
+    }
+  },
+  methods: {
+    handleClick() {
+      this.$refs.name.classList.toggle('active')
+    },
+    toggleModal() {
+      this.showModal = !this.showModal
+    }
+  }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  text-align: center;
-  margin-top: 60px;
-}
-</style>
---
+++ modal-project/src/components/Modal.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="backdrop" @click.self="closeModal">
+    <div class="modal" :class="{sale: theme === 'sale'}">
+      <slot>default content</slot>
+      <div class="actions">
+        <slot name="links"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Modal',
+  props: [
+    'theme',
+  ],
+  methods: {
+    closeModal() {
+      this.$emit('close')
+    }
+  }
+}
+</script>
+
+<style scoped>
+.modal {
+  width: 300px;
+  padding: 20px;
+  margin: 50px auto;
+  background: darkcyan;
+  color: white;
+  border: 2px solid white;
+  border-radius: 20px;
+}
+.modal.sale {
+  background: pink;
+}
+.backdrop {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  top: 0;
+  position: fixed;
+  background: rgba(0,0,0,0.4);
+  width: 100%;
+  height: 100%;
+}
+</style>
