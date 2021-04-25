c0489bc96be7c0f4ddefe1cafc3e9f7dee24b27d
Justyna Biernacka <justyna.biernacka.1986@gmail.com>
2021-04-25 18:33:02 +0200

    Add fake server & fetch data with axios

--- real-world-vue/src/views/EventList.vue
+++ real-world-vue/src/views/EventList.vue
@@ -1,16 +1,31 @@
 <template>
   <div>
     <h1>Event Listing</h1>
-    <EventCard />
+    <EventCard v-for="e in events" :key="e.id" :event="e" />
   </div>
 </template>
 
 <script>
 import EventCard from "@/components/EventCard.vue";
+import EventService from "@/services/EventService";
 
 export default {
+  data() {
+    return {
+      events: [],
+    };
+  },
   components: {
     EventCard,
+  },
+  created() {
+    EventService.getEvents()
+      .then((res) => {
+        this.events = res.data;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   },
 };
 </script>
