84b12027b84d8fc20935d3803363bd71cbcda3c2
Justyna Biernacka <justyna.biernacka.1986@gmail.com>
2021-04-25 14:32:27 +0200

    Add global component BaseIcon

---
+++ real-world-vue/src/components/BaseIcon.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="icon-wrapper">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="icon"
+    >
+      <polyline points="22 12 18 12 15 21 9 3 6 12 2 12"></polyline>
+    </svg>
+  </div>
+</template>
+
+<style scoped>
+.icon-wrapper {
+  display: inline-flex;
+  align-items: center;
+  color: rgba(0, 0, 0, 0.4);
+  font-size: 1rem;
+  font-weight: 600;
+  margin-right: 6px;
+}
+.icon {
+  stroke: currentColor;
+  stroke-width: 2;
+  stroke-linecap: round;
+  stroke-linejoin: round;
+  fill: none;
+}
+</style>
